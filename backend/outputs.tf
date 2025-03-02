f88a1e36d6d939f8d96a57fdd1e72dc711b30b53
Vishwanathshebinakatti <96469952+Vishwanathshebinakatti@users.noreply.github.com>
2025-03-02 16:06:04 +0530

    Update outputs.tf

--- backend/outputs.tf
+++ backend/outputs.tf
@@ -1,9 +1,9 @@
 output "aws_s3_bucket_name" {
-  value = aws_s3_bucket.terraform-s3
+  value = aws_s3_bucket.terraform-s3.id
   description = "name of the backend s3 bucket"
 }
 
 output "aws_dynamodb_table_name" {
-  value = aws_dynamodb_table.terraform-dynamodb-table
+  value = aws_dynamodb_table.terraform-dynamodb-table.id
   description = "Name of the backend dynamodb table"
 }
