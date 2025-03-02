e2e0a7f6cba6e91561045c659e695e75eb485c13
vishwa <svishwads@gmail.com>
2025-03-02 13:32:29 +0530

    s3_versioning_and_encryption

--- backend/main.tf
+++ backend/main.tf
@@ -9,6 +9,22 @@ resource "aws_s3_bucket" "terraform-s3" {
     prevent_destroy = false
   }
 }
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform-s3.id
+  versioning_configuration {
+    status="enabled"
+  } 
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
+  bucket=aws_s3_bucket.terraform-s3.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+  
+}
 
 resource "aws_dynamodb_table" "terraform-dynamodb-table" {
   name           = "demo-tf-test-dynamo-table"
